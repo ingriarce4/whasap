b084f81092e4e496f18cc27e89e5054d4b28d424
susana arce <ingridarce4@gmail.com>
2017-07-18 10:59:02 -0700

    previos

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+var input = document.write.getElementById(input);
+var imprimir = document.
+
+function Buscar{
+	if input (press click(function(event) {
+		/* Act on the event */
+	});)
+		alert mensaje ()
+}
